e44f646d5afa866dad8d2b6ff1cbb83be4d7eeb9
Tom Bee <tburzynski2@yahoo.com>
2024-04-11 17:30:34 -0500

    Got city geolocation coordinates & daily weather data from API

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,15 +2,64 @@ let cityInputEl = $("#city-input");
 let submitButton = $("#submit-button");
 const key = "8082a0eacc4e1bbc29b1dd51ac82e102";
 
-function getCityId(cityName) {}
+function getInput() {
+  return cityInputEl.val();
+}
 
-function getDailyForecast(cityId) {}
+function getCityLocation(cityName) {
+  let endpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${key}`;
+  return fetch(endpoint)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json().then(function (data) {
+          const lat = data[0].lat;
+          const lon = data[0].lon;
+          const result = {
+            lat: lat,
+            lon: lon,
+          };
+          console.log(result);
+          return result;
+        });
+      } else {
+        throw new Error(`Error:${response.statusText}`);
+      }
+    })
+    .then(function (location) {
+      return getDailyForecast(location.lat, location.lon);
+    })
+    .catch(function (error) {
+      alert("Unable to connect to OpenWeather API");
+      throw error; // rethrowing the error for further handling if necessary
+    });
+}
 
-function getFiveDayForecast(cityId) {}
+function getDailyForecast(lat, lon) {
+  const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
+  fetch(endpoint)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          console.log(
+            `Temp: ${data.main.temp}°F\nWind: ${data.wind.speed} MPH\nHumidity: ${data.main.humidity}%\nhttp://openweathermap.org/img/w/${data.weather[0].icon}.png`
+          );
+        });
+      } else {
+        alert(`Error:${response.statusText}`);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to OpenWeather API");
+    });
+}
+
+function getFiveDayForecast(lat, lon) {}
 
 $(document).ready(function () {
   submitButton.on("click", function (event) {
     event.preventDefault();
-    console.log(cityInputEl.val());
+    const cityName = getInput();
+    const geoLocation = getCityLocation(cityName);
   });
 });
